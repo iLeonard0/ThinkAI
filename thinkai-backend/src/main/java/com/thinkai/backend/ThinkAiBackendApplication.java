7cb2974b14b651f44232db02e06b5e26ecaabbe4
iLeonard0 <folleleonardoprogramming@gmail.com>
2025-09-21 20:21:42 -0300

    feat: add thinkAI-backend java

---
+++ thinkai-backend/src/main/java/com/thinkai/backend/ThinkAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.thinkai.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinkAiBackendApplication.class, args);
+	}
+
+}
---
+++ thinkai-backend/src/test/java/com/thinkai/backend/ThinkAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkai.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
