a7f870f923addeb28a30f8e26492ead557843d1e
iLeonard0 <folleleonardoprogramming@gmail.com>
2025-09-30 21:16:07 -0300

    chore: finish JWT login/register

---
+++ thinkai-backend/src/main/java/com/thinkai/backend/dto/LoginRequestDTO.java
@@ -0,0 +1,3 @@
+package com.thinkai.backend.dto;
+
+public record LoginRequestDTO(String email, String password) { }
---
+++ thinkai-backend/src/main/java/com/thinkai/backend/dto/ResponseDTO.java
@@ -0,0 +1,3 @@
+package com.thinkai.backend.dto;
+
+public record ResponseDTO(String name, String token) { }
