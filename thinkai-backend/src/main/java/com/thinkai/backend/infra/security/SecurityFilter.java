80db5f3f9eb8a078ceb1c8f7a3ed75db2e3e13a8
iLeonard0 <folleleonardoprogramming@gmail.com>
2025-10-06 20:49:34 -0300

    chore: remove a few warnings

--- thinkai-backend/src/main/java/com/thinkai/backend/infra/security/SecurityFilter.java
+++ thinkai-backend/src/main/java/com/thinkai/backend/infra/security/SecurityFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -28,7 +29,11 @@ public class SecurityFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException
+    {
         var token = this.recoverToken(request);
         var login = tokenService.validateToken(token);
 
